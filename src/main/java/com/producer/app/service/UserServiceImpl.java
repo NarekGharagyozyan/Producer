af82ee177291f6a99768b55d6cbf3d127430b69f
NarekGharagyozyan <narekgharagyozyann@gmail.com>
2024-03-15 18:35:52 +0400

    Adding logging logic.

--- src/main/java/com/producer/app/service/UserServiceImpl.java
+++ src/main/java/com/producer/app/service/UserServiceImpl.java
@@ -3,9 +3,11 @@ package com.producer.app.service;
 import com.producer.app.model.dto.user.UserRequestDto;
 import com.producer.app.service.producer.ProducerServiceUser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -16,6 +18,7 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public void registration(UserRequestDto userRequestDto) {
         producerServiceUser.sendMessage(userRequestDto);
+        log.info("userRequestDto with username: {} sent to Kafka.",userRequestDto.getUsername());
     }
 
 }
